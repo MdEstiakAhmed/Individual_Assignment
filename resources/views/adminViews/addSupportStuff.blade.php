4c02d6d4eead2dc4204a764d1640ef4a9cabf239
MdEstiakAhmed <estiak97@gmail.com>
2020-04-12 21:28:03 +0600

    M10F2: form for add support stuff

--- resources/views/adminViews/addSupportStuff.blade.php
+++ resources/views/adminViews/addSupportStuff.blade.php
@@ -14,5 +14,38 @@
     <br> -->
     <a href="/logout">logout</a>
     <br><br>
+
+    <form method="post">
+        @csrf
+        <label for="name">name:</label><br>
+        <input type="text" name="name" id="name" placeholder="enter your name">
+        @error('name')
+            <span>{{$message}}</span>
+        @enderror
+        <br><br>
+
+        <label for="email">email:</label><br>
+        <input type="text" name="email" id="email" placeholder="enter your email">
+        @error('email')
+            <span>{{$message}}</span>
+        @enderror
+        <br><br>
+
+        <label for="password">password:</label><br>
+        <input type="password" name="password" id="password" placeholder="enter your password">
+        @error('password')
+            <span>{{$message}}</span>
+        @enderror
+        <br><br>
+
+        <label for="confirmPassword">re-type password:</label><br>
+        <input type="password" name="confirmPassword" id="confirmPassword" placeholder="re-type password">
+        @error('confirmPassword')
+            <span>{{$message}}</span>
+        @enderror
+        <br><br>
+
+        <input type="submit" name="submit" value="add support stuff">
+    </form>
 </body>
 </html>
