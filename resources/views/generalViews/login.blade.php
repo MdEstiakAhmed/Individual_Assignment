e5569e876a67a98e5a22b50109bf363e466ef83a
MdEstiakAhmed <estiak97@gmail.com>
2020-04-12 08:11:45 +0600

    M1F1: login page create

---
+++ resources/views/generalViews/login.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login page</title>
+</head>
+<body>
+    <h1>Login Page</h1>
+    <form action="" method="post">
+        <label for="email">email:</label><br>
+        <input type="email" name="email" id="email" placeholder="enter your email">
+        <span id="emailError"></span>
+        <br><br>
+        <label for="password">password:</label><br>
+        <input type="password" name="password" id="password" placeholder="enter your password">
+        <span id="passwordError"></span>
+        <br><br>
+        <input type="submit" value="login">
+        <br><br>
+    </form>
+</body>
+</html>
